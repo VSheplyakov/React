baedda2b600cf0bfa9088a18e607a55c1f2e0895
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-09-27 16:48:25 +0300

    format App,.js

--- 11-state/src/App.js
+++ 11-state/src/App.js
@@ -4,7 +4,7 @@ import RandomNumber from './components/RandomNumber';
 function App() {
   return (
     <div className="App">
-      <RandomNumber maxNum={1000}/>
+      <RandomNumber maxNum={1000} />
     </div>
   );
 }
