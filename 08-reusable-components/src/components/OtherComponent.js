af4ec9c2b7c4c2a2180ffb8d139ae8f706055a92
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-09-27 14:59:12 +0300

    create new components

---
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,11 @@
+function OtherComponent() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni deleniti
+        ab possimus tenetur facere!
+      </p>
+    </div>
+  );
+}
+export default OtherComponent;
